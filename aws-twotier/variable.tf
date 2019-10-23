e3d7ce2cd293d3840df0c4c5a40e236d8318c35e
kaustubh <kaustubhin@gmail.com>
2019-10-23 15:26:03 +0530

    up password

--- aws-twotier/variable.tf
+++ aws-twotier/variable.tf
@@ -4,6 +4,6 @@ variable "aws_region" {
 variable "key_pair_path" {
   default = "myserverkey.pem"
 }
-variable "ADMIN_USER" {}
-variable "ADMIN_PASSWORD" {}
+variable "ADMIN_USER" { default = "Admin"}
+variable "ADMIN_PASSWORD" {default = "Passw0rd@12345"}
 
